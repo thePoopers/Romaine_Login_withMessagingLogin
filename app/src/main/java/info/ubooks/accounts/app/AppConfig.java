c22bc8c0ee0f2373ccc9f7fcf293497ef68beae9
Michael Romaine <megarockbass@gmail.com>
2015-04-20 02:44:50 -0400

    UBooks Account
    Account login/register system for UBooks

---
+++ app/src/main/java/info/ubooks/accounts/app/AppConfig.java
@@ -0,0 +1,9 @@
+package info.ubooks.accounts.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://theartistandtheengineer.co/ubooks/";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://theartistandtheengineer.co/ubooks/";
+}
